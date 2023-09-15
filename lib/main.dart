5c9da949e843194118d2ce3106095c030f089cfa
MrElnurr <elnurkerimli123@gmail.com>
2023-09-15 21:04:28 +0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,35 +14,47 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {
-                  var yoxlama =
-                      ref.read(userProvider.notifier).ourData.toString();
-                  debugPrint('$yoxlama');
-                },
-                icon: Icon(Icons.delete_sweep_outlined)),
-          ],
-          title: const Text('Material App Bar'),
-        ),
-        body: SafeArea(
-          child: ListView.builder(
-            itemCount: ref.watch(userProvider).length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: Text(ref.watch(userProvider)[index].id.toString()),
-                title: Text(ref.watch(userProvider)[index].title),
-                trailing: IconButton(
-                    onPressed: () {
-                      ref.read(userProvider.notifier).deleteItem(index);
-                    },
-                    icon: Icon(Icons.delete_rounded)),
-              );
-            },
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    var yoxlama =
+                        ref.read(userProvider.notifier).ourData.toString();
+                    debugPrint('$yoxlama');
+                  },
+                  icon: Icon(Icons.delete_sweep_outlined)),
+            ],
+            title: const Text('Material App Bar'),
           ),
-        ),
-      ),
+          body: FutureBuilder(
+            future: ref.read(userProvider.notifier).getUsers(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                // ignore: avoid_print
+                print(snapshot.error.toString());
+                return Container();
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Text(snapshot.data![index].id.toString()),
+                      title: Text(snapshot.data![index].title),
+                      trailing: IconButton(
+                          onPressed: () {
+                            ref.watch(userProvider.notifier).deleteItem(index);
+                          },
+                          icon: Icon(Icons.delete_rounded)),
+                    );
+                  },
+                );
+              }
+            },
+          )),
     );
   }
 }
